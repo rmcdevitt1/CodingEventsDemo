e2f74be13852258c1fdd6b3dbe1701bff3244fd6
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-10-12 20:11:50 -0500

    19.4.4 DONE

---
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CodingEventsDemo.Data;
+using CodingEventsDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEventsDemo.Controllers
+{
+    public class EventCategoryController : Controller
+    {
+        private EventDbContext context;
+
+        public EventCategoryController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        [HttpGet]
+        [Route("EventCategory/Index")]
+        public IActionResult Index()
+        {
+            List<EventCategory> categories = context.Categories.ToList();
+
+            return View(categories);
+        }
+
+
+
+    }
+}
