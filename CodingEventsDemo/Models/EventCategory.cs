ff1352919a81e8b19043c3aed345acf31b6fe4b7
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-10-12 20:00:23 -0500

    19.4.3 DONE

--- CodingEventsDemo/Models/EventCategory.cs
+++ CodingEventsDemo/Models/EventCategory.cs
@@ -17,5 +17,21 @@ namespace CodingEventsDemo.Models
         {
             Name = name;
         }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Event @event &&
+                   Id == @event.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
     }
 }
