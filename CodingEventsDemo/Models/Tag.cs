4b5dcc4f245f048120006c8bdf03739b96c9e417
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-10-18 17:55:40 -0500

    added Tag Model

---
+++ CodingEventsDemo/Models/Tag.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace CodingEventsDemo.Models
+{
+    public class Tag
+    {
+        public string Name { get; set; }
+
+        public int Id { get; set; }
+
+        public Tag(string name)
+        {
+            Name = name;
+        }
+
+        public Tag()
+        {
+        }
+    }
+}
