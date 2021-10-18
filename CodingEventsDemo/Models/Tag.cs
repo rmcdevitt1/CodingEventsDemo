19c57d9b4f824102d126a1cbc5e2be8dc90cf000
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-10-18 18:17:19 -0500

    20.4

--- CodingEventsDemo/Models/Tag.cs
+++ CodingEventsDemo/Models/Tag.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace CodingEventsDemo.Models
 {
     public class Tag
     {
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters long.")]
         public string Name { get; set; }
 
         public int Id { get; set; }
