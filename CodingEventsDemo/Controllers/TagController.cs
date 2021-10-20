255fefb52f99098fbc1109f2d36b012962313aac
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-10-19 20:46:02 -0500

    updated tagController

--- CodingEventsDemo/Controllers/TagController.cs
+++ CodingEventsDemo/Controllers/TagController.cs
@@ -46,5 +46,40 @@ namespace CodingEventsDemo.Controllers
 
             return View("Add", tag);
         }
+
+        // responds to URLs like /Tag/AddEvent/5 (where 5 is an event ID)
+        public IActionResult AddEvent(int id)
+        {
+            Event theEvent = context.Events.Find(id);
+            List<Tag> possibleTags = context.Tags.ToList();
+            AddEventTagViewModel viewModel = new AddEventTagViewModel(theEvent, possibleTags);
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult AddEvent(AddEventTagViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+
+                int eventId = viewModel.EventId;
+                int tagId = viewModel.TagId;
+
+                EventTag eventTag = new EventTag
+                {
+                    EventId = eventId,
+                    TagId = tagId
+                };
+                context.EventTags.Add(eventTag);
+                context.SaveChanges();
+
+                return Redirect("/Events/Detail/" + eventId);
+            }
+
+            return View(viewModel);
+        }
+
+
+
     }
 }
