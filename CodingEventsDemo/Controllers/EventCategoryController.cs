55843fc6b18b76734a4ee1744ad004164a36ad7f
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-11-03 13:32:36 -0500

    begin follow-along

--- CodingEventsDemo/Controllers/EventCategoryController.cs
+++ CodingEventsDemo/Controllers/EventCategoryController.cs
@@ -7,8 +7,6 @@ using CodingEventsDemo.Models;
 using Microsoft.AspNetCore.Mvc;
 using CodingEventsDemo.ViewModels;
 
-//:)
-
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace CodingEventsDemo.Controllers
