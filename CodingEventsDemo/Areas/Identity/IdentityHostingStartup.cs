72ef7d60fa9bf94a1e37a8214aa54f8c77b78c54
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-11-03 16:47:15 -0500

    keyword userid not supported error

--- CodingEventsDemo/Areas/Identity/IdentityHostingStartup.cs
+++ CodingEventsDemo/Areas/Identity/IdentityHostingStartup.cs
@@ -1,5 +1,6 @@
 using System;
-using CodingEventsDemo.Areas.Identity.Data;
+using CodingEventsDemo.Data;
+//using CodingEventsDemo.Areas.Identity.Data;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
