16f2ded65612145f0ed81e74ac966ebc38ff24e5
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-11-03 19:47:04 -0500

    21.3

--- CodingEventsDemo/Startup.cs
+++ CodingEventsDemo/Startup.cs
@@ -26,8 +26,9 @@ namespace CodingEventsDemo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddDbContext<EventDbContext>(options =>
-                options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddRazorPages();
+            //services.AddDbContext<EventDbContext>(options =>
+            //    options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,8 +54,9 @@ namespace CodingEventsDemo
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                   name: "default",
+                   pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
