942d1d39c04986543b17e6689ab2a077819c241c
Raine McDevitt <raine.mcdevitt@gmail.com>
2021-11-17 21:26:56 -0600

    DONE

--- CodingEventsDemo/Startup.cs
+++ CodingEventsDemo/Startup.cs
@@ -63,6 +63,7 @@ namespace CodingEventsDemo
                    pattern: "{controller=Home}/{action=Index}/{id?}");
                 endpoints.MapRazorPages();
             });
+
         }
     }
 }
